54a57ecaad3fe0146b7a56fa3a31cd99ef4afe64
TheWoomy27 <austin.fares27@gmail.com>
2026-03-11 21:38:21 -0400

    Dotfiles Sync

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -5,7 +5,7 @@ source ~/.config/fish/laptop.fish
 alias dotfiles='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
 alias cmatrix='cmatrix -C blue'
 set -gx NEWT_COLORS "root=#c8d3f5,#222436;border=#131421,#1e2030;window=#c8d3f5,#1e2030;shadow=#222436,#222436;title=#c8d3f5,#222436;button=#c8d3f5,#1e2030;actbutton=#c8d3f5,#444a73;checkbox=black,#c8d3f5;actcheckbox=#c8d3f5,#444a73;entry=#c8d3f5,#1e2030;label=#c8d3f5,#1e2030;listbox=#c8d3f5,#1e2030;actlistbox=#7cafff,#1e2030;textbox=#c8d3f5,#1e2030;acttextbox=#c8d3f5, #131421;helpline=#131421,#1e2030;roottext=#131421,#1e2030;emptyscale=red,#c8d3f5;fullscale=green,#c8d3f5;disabled_entry=gray,#c8d3f5;compactbutton=#c8d3f5,#131421;actsellistbox=#d5def8,#444a73;sellistbox=black,#444a73'   nmtui"
-alias vencord='kill $(pidof discord) && sh -c "$(curl -sS https://vencord.dev/install.sh)" && discord & disown'
+alias vencord='kill $(pidof discord) && sh -c "$(curl -sS https://vencord.dev/install.sh)"'
   
 # eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv fish)"
 
